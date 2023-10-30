b29e21ae62d99dc3a7e3986e144f95f0c5458465
syaifulalam046 <syaifulalam046@gmail.com>
2023-10-30 15:42:30 +0800

    login, register, forgot password

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; simpadu-c030322137
+  </div>
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.app.dashboard-simpadu', ['type_menu' => '']);
+});
+
+Route::get('/login', function () {
+
+    return view('pages.auth.auth-login');
+
+});
+
+Route::get('/register', function () {
+
+    return view('pages.auth.auth-register');
+
+})->name('register');
+
+Route::get('/forgot', function () {
+
+    return view('pages.auth.auth-forgot-password');
+
+})->name('forgot');
+
+Route::get('/reset', function () {
+
+    return view('pages.auth.auth-reset-password');
+
+})->name('reset');
